11c5f3bf51e7eec73c79b032a1a760b7dc7e5498
marepal631 <marepal631@student.polsl.pl>
2023-12-27 15:20:49 +0100

    logger changes

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,8 +2,8 @@ import {NextFunction, Request, Response} from "express";
 
 export const logInvokedEndpoint = (req: Request, res: Response, next: NextFunction) => {
     const now: Date = new Date()
-    const date: string = now.toDateString()
+    const date: string = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`
     const time: string = now.toTimeString().slice(0, 8)
-    console.log(`[${date}, ${time}]`, req.method, req.url, 'was invoked')
+    console.log(`[${date} ${time}]`, req.method, req.originalUrl, 'was invoked')
     next()
 }
