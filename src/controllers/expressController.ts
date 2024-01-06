761e9b7c20f7fc322e116d10a47819d32c98c348
unuel <noob7007@gmail.com>
2024-01-06 21:46:30 +0100

    Actual error logger part

--- src/controllers/expressController.ts
+++ src/controllers/expressController.ts
@@ -1,7 +1,19 @@
-import express, {Express} from "express";
+import express, {Express, Request, Response, NextFunction} from "express";
 import {router as rolesRouter} from "../routes/roleRoutes";
+import { ResponseError } from "../utils/response";
+import { logError } from "../utils/logger";
 
 export const app: Express = express()
 
 app.use(express.json())
 app.use('/roles', rolesRouter)
+
+app.use(function(err: (ResponseError | Error), req: Request, res: Response, next: NextFunction){
+    logError(err)
+    if(err instanceof ResponseError){
+        res.status(err.status)
+    } else{
+        res.status(500)
+    }
+    res.json({message: err.message})
+})
