5ee27c773e4d7535ebbdcaed04315cf18ce99491
marepal631 <marepal631@student.polsl.pl>
2024-02-11 12:03:19 +0100

    minor refactor

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,6 @@ import "./utils/overrides";
 import {app} from "./controllers/expressController";
 import "dotenv/config"
 
-const server = app
-
-server.listen(process.env.PORT || 8080, () => {
+app.listen(process.env.PORT || 8080, () => {
     console.log(`Server is listening on port: ${process.env.PORT || 8080}`)
 })
