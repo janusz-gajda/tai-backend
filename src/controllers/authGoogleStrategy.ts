9c917f44b5a8ce282d93c80937b1fa7c768dc2d2
marepal631 <marepal631@student.polsl.pl>
2024-02-12 22:34:38 +0100

    google oauth2 changes

--- src/controllers/authGoogleStrategy.ts
+++ src/controllers/authGoogleStrategy.ts
@@ -1,8 +1,8 @@
 import {User} from "@prisma/client";
 import {findUserByGoogleId, setGoogleIdToExistingUserOrCreateNewUser} from "../repositories/userRepository";
 import {generateRandomPassword} from "./userController";
-import {google} from "googleapis";
 import axios from "axios";
+import * as querystring from "querystring";
 
 type UserInfo = {
     sub: string,
@@ -14,20 +14,31 @@ type UserInfo = {
     locale: string
 }
 
-const oauth2Client = new google.auth.OAuth2(
-    process.env.GOOGLE_CLIENT_ID as string,
-    process.env.GOOGLE_CLIENT_SECRET as string,
-    `http://localhost:${process.env.PORT}/auth/google/callback`
-)
+// const oauth2Client = new google.auth.OAuth2(
+//     process.env.GOOGLE_CLIENT_ID as string,
+//     process.env.GOOGLE_CLIENT_SECRET as string,
+//     `http://localhost:${process.env.PORT}/auth/google/callback`
+// )
 
 // only for test purposes
-export const authorizationUrl = oauth2Client.generateAuthUrl({
-    scope: ['profile', 'email']
-})
+// export const authorizationUrl = oauth2Client.generateAuthUrl({
+//     scope: ['profile', 'email']
+// })
 
 export async function getAccessToken(code: string) {
-    const tokens = await oauth2Client.getToken(code)
-    return tokens.tokens.access_token
+    const options = {
+        code,
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        client_secret: process.env.GOOGLE_CLIENT_SECRET,
+        redirect_uri: `http://localhost:${process.env.PORT}/auth/google/callback`,
+        grant_type: 'authorization_code'
+    }
+    const tokens = await axios.post('https://oauth2.googleapis.com/token', querystring.stringify(options), {
+        headers: {
+            "Content-Type": 'application/x-www-form-urlencoded'
+        }
+    })
+    return tokens.data.access_token
 }
 
 export async function getUserData(accessToken: string) {
